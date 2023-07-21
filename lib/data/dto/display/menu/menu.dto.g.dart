6c21de80a293056c53437e9677e12101c2c4d6e1
choi-jae-woong <jason967@naver.com>
2023-07-22 02:09:10 +0900

    menu dto 추가

---
+++ lib/data/dto/display/menu/menu.dto.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'menu.dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_MenuDto _$$_MenuDtoFromJson(Map<String, dynamic> json) => _$_MenuDto(
+      tabId: json['tabId'] as int? ?? -1,
+      title: json['title'] as String? ?? '',
+    );
+
+Map<String, dynamic> _$$_MenuDtoToJson(_$_MenuDto instance) =>
+    <String, dynamic>{
+      'tabId': instance.tabId,
+      'title': instance.title,
+    };
