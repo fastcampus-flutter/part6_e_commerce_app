19fb6f865122c19c2982d52fad63d71a73db89b3
choi-jae-woong <jason967@naver.com>
2023-07-22 00:52:56 +0900

    cubit을 이용한 top appbar 구현

--- lib/presentation/main/component/top_app_bar/default_app_bar.dart
+++ lib/presentation/main/component/top_app_bar/default_app_bar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../cubit/bottom_nav_cubit.dart';
+import '../../cubit/mall_type_cubit.dart';
 
 class DefaultAppBar extends StatelessWidget {
   const DefaultAppBar(this.bottomNav, {super.key});
@@ -9,20 +11,28 @@ class DefaultAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 8),
-      color: Theme.of(context).colorScheme.primary,
-      child: AppBar(
-        title: Text(
-          bottomNav.toName,
-          style: Theme.of(context)
-              .textTheme
-              .headlineSmall
-              ?.copyWith(color: Colors.white),
-        ),
-        backgroundColor: Colors.transparent,
-        centerTitle: true,
-      ),
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return BlocBuilder<MallTypeCubit, MallType>(
+      builder: (_, state) {
+        return AnimatedContainer(
+          padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 8),
+          color: state.isMarket ? colorScheme.primary : colorScheme.background,
+          child: AppBar(
+            title: Text(
+              bottomNav.toName,
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                    color: state.isMarket
+                        ? colorScheme.background
+                        : colorScheme.primary,
+                  ),
+            ),
+            backgroundColor: Colors.transparent,
+            centerTitle: true,
+          ),
+          duration: Duration(milliseconds: 400),
+        );
+      },
     );
   }
 }
