29af3b0cfffe7955b2c4237f339547f8fafeb09b
choi-jae-woong <jason967@naver.com>
2023-07-23 01:30:16 +0900

    view_module_list 수정

--- lib/presentation/pages/home/component/view_module_list/view_module_list.dart
+++ lib/presentation/pages/home/component/view_module_list/view_module_list.dart
@@ -1,30 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../../../core/utils/constant.dart';
+import '../../../../../core/utils/extensions.dart';
 import '../../bloc/view_module_bloc/view_module_bloc.dart';
 
-class ViewModuleList extends StatelessWidget {
+class ViewModuleList extends StatefulWidget {
   const ViewModuleList({super.key});
+
+  @override
+  State<ViewModuleList> createState() => _ViewModuleListState();
+}
+
+class _ViewModuleListState extends State<ViewModuleList> {
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(_onScroll);
+  }
+
+  void _onScroll() {
+    if (_isEnd) {
+      context.read<ViewModuleBloc>().add(ViewModuleFetched());
+    }
+  }
+
+  @override
+  void dispose() {
+    scrollController
+      ..removeListener(_onScroll)
+      ..dispose();
+    super.dispose();
+  }
+
+  bool get _isEnd {
+    if (!scrollController.hasClients) return false;
+    final maxScroll = scrollController.position.maxScrollExtent;
+    final curScroll = scrollController.offset;
+
+    return curScroll >= maxScroll * (0.9);
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ViewModuleBloc, ViewModuleState>(
       builder: (_, state) {
-        switch (state.status) {
-          case Status.initial:
-          case Status.loading:
-            return const Center(child: CircularProgressIndicator());
-          case Status.success:
-            return ListView.separated(
-              itemBuilder: (_, index) => state.viewModules[index],
-              separatorBuilder: (_, index) => Divider(thickness: 4),
-              itemCount: state.viewModules.length,
-            );
-          case Status.error:
-            return const Center(child: Text('view_module_bloc error'));
-        }
+        return (state.status.isInitial || state.viewModules.isEmpty)
+            ? LoadingWidget()
+            : ListView(
+                controller: scrollController,
+                children: [
+                  ...state.viewModules,
+                  if (state.status.isLoading)
+                    const LoadingWidget(isBottom: true),
+                ],
+              );
       },
     );
+  }
+}
+
+class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({this.isBottom = false, super.key});
+
+  final bool isBottom;
+
+  @override
+  Widget build(BuildContext context) {
+    return (isBottom)
+        ? SizedBox(
+            height: 50,
+            child: Center(
+              child: SizedBox(
+                width: 20,
+                height: 20,
+                child: CircularProgressIndicator(strokeWidth: 2),
+              ),
+            ),
+          )
+        : Center(
+            child: CircularProgressIndicator(),
+          );
   }
 }
