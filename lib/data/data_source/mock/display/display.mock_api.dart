d29d8216d6669d997a6ad42c6a36aa4027626aa3
choi-jae-woong <jason967@naver.com>
2023-07-22 15:38:02 +0900

    mock_data 추가, mock_api,api 추가

--- lib/data/data_source/mock/display/display.mock_api.dart
+++ lib/data/data_source/mock/display/display.mock_api.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 
 import '../../../dto/common/response_wrapper/response_wrapper.dart';
-import '../../../dto/display/menu/menu.dto.dart';
+
+import '../../../dto/display/display.dto.dart';
 import '../../remote/display/display.api.dart';
 import 'display_mock_data.dart';
 
@@ -31,6 +32,35 @@ class DisplayMockApi implements DisplayApi {
     );
   }
 
+  @override
+  Future<ResponseWrapper<List<ViewModuleDto>>> getViewModulesByTabId(
+    int tabId,
+  ) {
+    late String source;
+    final endOfTabId = tabId % 10;
+    switch (endOfTabId) {
+      case 1:
+        source = DisplayMockData.viewModulesByTabIdCaseOne;
+      case 2:
+        source = DisplayMockData.viewModulesByTabIdCaseTwo;
+      case 3:
+        source = DisplayMockData.viewModulesByTabIdCaseThree;
+      case 4:
+        source = DisplayMockData.viewModulesByTabIdCaseFour;
+      case 5:
+        source = DisplayMockData.viewModulesByTabIdCaseFive;
+    }
+
+    return Future(
+      () => ResponseWrapper(
+        status: 'SUCCESS',
+        code: '0000',
+        message: '',
+        data: viewModuleParser(source),
+      ),
+    );
+  }
+
   // parsers
   List<MenuDto> menuParser(String source) {
     List<MenuDto> menus = [];
@@ -38,5 +68,13 @@ class DisplayMockApi implements DisplayApi {
     menus = json.map((e) => MenuDto.fromJson(e)).toList();
 
     return menus;
+  }
+
+  List<ViewModuleDto> viewModuleParser(String source) {
+    List<ViewModuleDto> viewModules = [];
+    final List json = jsonDecode(source);
+    viewModules = json.map((e) => ViewModuleDto.fromJson(e)).toList();
+
+    return viewModules;
   }
 }
--- lib/data/data_source/mock/display/display_mock_data.dart
+++ lib/data/data_source/mock/display/display_mock_data.dart
@@ -4,4 +4,14 @@ class DisplayMockData {
 
   static final String menusByBeauty =
       '[{"title":"패캠추천","tabId":20001},{"title":"페스티벌","tabId":20002},{"title":"신상품","tabId":20003},{"title":"베스트","tabId":20004},{"title":"브랜드관","tabId":20005}]';
+  static final String viewModulesByTabIdCaseOne =
+      '[{"type":"ViewModuleA"},{"type":"ViewModuleB"},{"type":"ViewModuleC"},{"type":"ViewModuleD"},{"type":"ViewModuleE"}]';
+  static final String viewModulesByTabIdCaseTwo =
+      '[{"type":"ViewModuleB"},{"type":"ViewModuleC"},{"type":"ViewModuleD"},{"type":"ViewModuleE"},{"type":"ViewModuleA"}]';
+  static final String viewModulesByTabIdCaseThree =
+      '[{"type":"ViewModuleC"},{"type":"ViewModuleD"},{"type":"ViewModuleE"},{"type":"ViewModuleA"},{"type":"ViewModuleB"}]';
+  static final String viewModulesByTabIdCaseFour =
+      '[{"type":"ViewModuleD"},{"type":"ViewModuleE"},{"type":"ViewModuleA"},{"type":"ViewModuleB"},{"type":"ViewModuleC"}]';
+  static final String viewModulesByTabIdCaseFive =
+      '[{"type":"ViewModuleE"},{"type":"ViewModuleA"},{"type":"ViewModuleB"},{"type":"ViewModuleC"},{"type":"ViewModuleD"}]';
 }
