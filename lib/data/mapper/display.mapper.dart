d054eb2e691367dfb4e64dbde6e11abbacdd8fe4
choi-jae-woong <jason967@naver.com>
2023-07-22 15:45:56 +0900

    mapper추가 , 리팩토링

--- lib/data/mapper/display.mapper.dart
+++ lib/data/mapper/display.mapper.dart
@@ -1,11 +1,22 @@
-import '../../domain/model/display/menu/menu.model.dart';
-import '../dto/display/menu/menu.dto.dart';
+import '../../domain/model/display/display.model.dart';
+import '../dto/display/display.dto.dart';
 
 extension MenuEx on MenuDto {
   Menu toModel() {
     return Menu(
       tabId: tabId ?? 0,
       title: title ?? '',
+    );
+  }
+}
+
+extension ViewModuleDtoEx on ViewModuleDto {
+  ViewModule toModel() {
+    return ViewModule(
+      type: type ?? '',
+      title: title ?? '',
+      subtitle: subtitle ?? '',
+      imageUrl: imageUrl ?? '',
     );
   }
 }
