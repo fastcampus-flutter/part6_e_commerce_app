abd81000b0200c2c5880bb8395f184e18f12661d
Shyunwook <gdnfl0117@gmail.com>
2023-07-24 18:08:52 +0900

    brand product viewModule 개발 완료

--- lib/presentation/pages/home/component/view_module_list/brand_product.view_module.dart
+++ lib/presentation/pages/home/component/view_module_list/brand_product.view_module.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:injectable/injectable.dart';
 
 import '../../../../../core/theme/constant/app_icons.dart';
 import '../../../../../core/theme/custom/custom_font_weight.dart';
 import '../../../../../core/theme/custom/custom_theme.dart';
+import '../../../../../core/utils/extensions.dart';
 import '../../../../../domain/model/display/display.model.dart';
+import '../../../../../domain/model/display/product_info/product_info.model.dart';
 import 'factory/view_module_widget.dart';
 import 'widget/view_module_padding.dart';
 import 'widget/view_module_title.dart';
@@ -70,10 +73,9 @@ class BrandProductViewModule extends StatelessWidget with ViewModuleWidget {
             physics: const NeverScrollableScrollPhysics(),
             shrinkWrap: true,
             itemBuilder: (_, index) {
-              /////////////////////////////////////
-              ///////////// 상품영역 //////////////
-              /////////////////////////////////////
-              return Container();
+              return _BrandProduct(
+                productInfo: info.products[index],
+              );
             },
             separatorBuilder: (_, __) {
               return const SizedBox(
@@ -122,6 +124,129 @@ class BrandProductViewModule extends StatelessWidget with ViewModuleWidget {
           ),
           const SizedBox(
             height: 16,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BrandProduct extends StatelessWidget {
+  final ProductInfo productInfo;
+
+  const _BrandProduct({
+    Key? key,
+    required this.productInfo,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var textStyle = Theme.of(context).textTheme;
+    var colorScheme = Theme.of(context).colorScheme;
+
+    return AspectRatio(
+      aspectRatio: 343 / 61,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 49,
+            height: 49,
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(
+                Radius.circular(5),
+              ),
+              child: Image.network(
+                productInfo.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 11,
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    productInfo.title,
+                    style: textStyle.labelLarge?.titleCopyWith(),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '${productInfo.discrountRate}%',
+                        style: textStyle.labelLarge?.discountRateCopyWith(),
+                      ),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                        productInfo.price.toWon(),
+                        style: textStyle.labelLarge?.priceCopyWith(),
+                      ),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                        productInfo.originalPrice.toWon(),
+                        style: textStyle.labelMedium?.originalPriceCopyWith(),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 5),
+            child: TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 10)
+                    .copyWith(left: 8),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                    color: colorScheme.outline,
+                  ),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(6.0),
+                  ),
+                ),
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              ),
+              child: Row(
+                children: [
+                  SvgPicture.asset(
+                    AppIcons.cart,
+                    width: 18,
+                    height: 18,
+                    colorFilter: ColorFilter.mode(
+                      colorScheme.contentPrimary,
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Text(
+                    '담기',
+                    style: textStyle.titleSmall
+                        ?.copyWith(
+                          color: colorScheme.contentPrimary,
+                        )
+                        .regular,
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
