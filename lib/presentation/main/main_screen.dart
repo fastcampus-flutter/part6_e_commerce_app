8fde5d5143c92f1b9710b8bfed59d158b2dfd42b
최재웅 <jason967@naver.com>
2023-07-25 15:21:06 +0900

    main_screen에 바텀 시트 등록

--- lib/presentation/main/main_screen.dart
+++ lib/presentation/main/main_screen.dart
@@ -6,9 +6,11 @@ import '../pages/category/category_page.dart';
 import '../pages/home/home_page.dart';
 import '../pages/search/search_page.dart';
 import '../pages/user/user_page.dart';
+import 'bloc/cart_bloc/cart_bloc.dart';
 import 'component/top_app_bar/top_app_bar.dart';
 import 'cubit/bottom_nav_cubit.dart';
 import 'cubit/mall_type_cubit.dart';
+import 'utils/bottom_sheet/cart_bottom_sheet/cart_bottom_sheet.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -32,19 +34,26 @@ class MainScreenView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const TopAppBar(),
-      body: BlocBuilder<BottomNavCubit, BottomNav>(
-        builder: (_, state) {
-          switch (state) {
-            case BottomNav.home:
-              return const HomePage();
-            case BottomNav.category:
-              return const CategoryPage();
-            case BottomNav.search:
-              return const SearchPage();
-            case BottomNav.user:
-              return const UserPage();
-          }
+      body: BlocListener<CartBloc, CartState>(
+        listener: (context, state) {
+          cartBottomSheet(context)
+              .whenComplete(() => context.read<CartBloc>().add(CartClosed()));
         },
+        listenWhen: (pre, cur) => pre.status.isClose && cur.status.isOpen,
+        child: BlocBuilder<BottomNavCubit, BottomNav>(
+          builder: (_, state) {
+            switch (state) {
+              case BottomNav.home:
+                return const HomePage();
+              case BottomNav.category:
+                return const CategoryPage();
+              case BottomNav.search:
+                return const SearchPage();
+              case BottomNav.user:
+                return const UserPage();
+            }
+          },
+        ),
       ),
       bottomNavigationBar: BlocBuilder<BottomNavCubit, BottomNav>(
         builder: (_, state) {
