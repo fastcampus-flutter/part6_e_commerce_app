36d5e5b1680d412e53a355b2b4f8ab6cd27b9358
최재웅 <jason967@naver.com>
2023-07-27 09:48:39 +0900

    hive entity 생성 및 등록

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'core/theme/theme_data.dart';
 
+import 'data/entity/display/cart/cart.entity.dart';
+import 'data/entity/display/product_info/product_info.entity.dart';
 import 'dependency_injection.dart';
 
 import 'presentation/main/bloc/cart_bloc/cart_bloc.dart';
@@ -14,6 +15,10 @@ void main() async {
   configureDependencies();
   // hive 초기화
   await Hive.initFlutter();
+
+  // hive object adapter
+  Hive.registerAdapter(ProductInfoEntityAdapter());
+  Hive.registerAdapter(CartEntityAdapter());
 
   runApp(const MainApp());
 }
