379700eb685db4c9f2543fda9909b9a0c6beb2c0
Shyunwook <gdnfl0117@gmail.com>
2023-07-31 23:06:49 +0900

    bloc 추가, bloc builder 추가

--- lib/dependency_injection.config.dart
+++ lib/dependency_injection.config.dart
@@ -12,7 +12,7 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:lecture_e_commerce/data/data_source/data_source_module.dart'
-    as _i13;
+    as _i16;
 import 'package:lecture_e_commerce/data/data_source/local_storage/display.dao.dart'
     as _i5;
 import 'package:lecture_e_commerce/data/data_source/remote/display/display.api.dart'
@@ -21,18 +21,24 @@ import 'package:lecture_e_commerce/data/repository_impl/display.repository_impl.
     as _i7;
 import 'package:lecture_e_commerce/domain/repository/display.repository.dart'
     as _i6;
+import 'package:lecture_e_commerce/domain/repository/use.repository.dart'
+    as _i12;
 import 'package:lecture_e_commerce/domain/usecase/display/display.usecase.dart'
     as _i8;
+import 'package:lecture_e_commerce/domain/usecase/user/user.usecase.dart'
+    as _i11;
 import 'package:lecture_e_commerce/presentation/main/bloc/cart_bloc/cart_bloc.dart'
     as _i3;
 import 'package:lecture_e_commerce/presentation/main/bloc/paymoent_bloc/payment_bloc.dart'
     as _i10;
+import 'package:lecture_e_commerce/presentation/main/bloc/user_bloc/user_bloc.dart'
+    as _i15;
 import 'package:lecture_e_commerce/presentation/pages/cart_list/bloc/cart_list_bloc/cart_list_bloc.dart'
-    as _i12;
+    as _i14;
 import 'package:lecture_e_commerce/presentation/pages/home/bloc/menu_bloc/menu_bloc.dart'
     as _i9;
 import 'package:lecture_e_commerce/presentation/pages/home/bloc/view_module_bloc/view_module_bloc.dart'
-    as _i11;
+    as _i13;
 
 extension GetItInjectableX on _i1.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -57,12 +63,14 @@ extension GetItInjectableX on _i1.GetIt {
         _i8.DisplayUsecase(gh<_i6.DisplayRepository>()));
     gh.factory<_i9.MenuBloc>(() => _i9.MenuBloc(gh<_i8.DisplayUsecase>()));
     gh.factory<_i10.PaymentBloc>(() => _i10.PaymentBloc());
-    gh.factory<_i11.ViewModuleBloc>(
-        () => _i11.ViewModuleBloc(gh<_i8.DisplayUsecase>()));
-    gh.factory<_i12.CartListBloc>(
-        () => _i12.CartListBloc(gh<_i8.DisplayUsecase>()));
+    gh.singleton<_i11.UserUsecase>(_i11.UserUsecase(gh<_i12.UserRepository>()));
+    gh.factory<_i13.ViewModuleBloc>(
+        () => _i13.ViewModuleBloc(gh<_i8.DisplayUsecase>()));
+    gh.factory<_i14.CartListBloc>(
+        () => _i14.CartListBloc(gh<_i8.DisplayUsecase>()));
+    gh.factory<_i15.UserBloc>(() => _i15.UserBloc(gh<_i11.UserUsecase>()));
     return this;
   }
 }
 
-class _$DataSourceModule extends _i13.DataSourceModule {}
+class _$DataSourceModule extends _i16.DataSourceModule {}
